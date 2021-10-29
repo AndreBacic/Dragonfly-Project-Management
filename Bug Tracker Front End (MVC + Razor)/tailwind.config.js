2877dd63154d9692ac6c504cb9dffde458c4dd26
andre9050@yahoo.com <andre9050@yahoo.com>
2021-10-28 22:06:46 -0400

    implementing tailwind and starting on new UI design

--- Bug Tracker Front End (MVC + Razor)/tailwind.config.js
+++ Bug Tracker Front End (MVC + Razor)/tailwind.config.js
@@ -9,7 +9,24 @@ module.exports = {
     ],
     darkMode: 'media', // false or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                blueGray: colors.blueGray,
+                coolGray: colors.coolGray,
+                trueGray: colors.trueGray,
+                warmGrey: colors.warmGray,
+                amber: colors.amber,
+                lime: colors.lime,
+                emerald: colors.emerald,
+                teal: colors.teal,
+                cyan: colors.cyan,
+                sky: colors.sky,
+                lightBlue: colors.lightBlue,
+                violet: colors.violet,
+                fuchsia: colors.fuchsia,
+                rose: colors.fuchsia
+            }
+        },
     },
     variants: {
         extend: {},
